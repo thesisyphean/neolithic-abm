0a1b010803fcdaba442a0116f23a3cecb336d46c
Ethan Wolff <thesisyphean256@gmail.com>
2023-10-05 13:26:25 +0200

    Arbitrary variables against iteration in plot.py

--- results/plot.py
+++ results/plot.py
@@ -6,11 +6,19 @@ def main():
     data = pd.read_csv("results/results.csv")
 
     sns.set_theme()
-
     plt.figure()
-    sns.lineplot(data=data, x="Iteration", y="Population")
-    plt.title(f"Population vs. Iteration")
-    plt.savefig("results/plot.png", dpi=300)
+
+    plot_vs_iteration(data, "Population")
+    plot_vs_iteration(data, "Cooperation")
+    plot_vs_iteration(data, "AveResources")
+    plot_vs_iteration(data, "MaxLoad")
+
+def plot_vs_iteration(data, variable):
+    plt.clf()
+    sns.lineplot(data=data, x="Iteration", y=variable)
+    plt.title(f"{variable} vs. Iteration")
+    plt.savefig(f"results/{variable.lower()}.png", dpi=300)
+    print(f"Plotted {variable}!")
 
 if __name__ == "__main__":
     main()
